0f752678487f910f7a11be3f8473a7ee63f58f2c
tabish2786 <iitian.tabish@gmail.com>
2026-02-12 21:40:19 +0530

    Add files via upload

---
+++ Bitwise.py
@@ -0,0 +1,8 @@
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+print("Bitwise AND =", a & b)
+print("Bitwise OR =", a | b)
+print("Bitwise XOR =", a ^ b)
+print("Right Shift =", a >> 1)
+print("Left Shift =", a << 1)
